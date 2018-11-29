8deb41ff2d298b489282e7976e072f161d549a03
marcelo.silva <marcelo.silva@ca-ti-16>
2018-11-29 17:09:15 -0200

    -remoção do comentário no inicio da página

--- src/main/java/br/com/backend/business/facade/CompanyFacade.java
+++ src/main/java/br/com/backend/business/facade/CompanyFacade.java
@@ -1,9 +1,3 @@
-/*
- * Copyright (C) Unimed São Carlos - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Marcelo Soares <marcelo.suares@yahoo.com.br>, Jun 2016
- */
 package br.com.backend.business.facade;
 
 import br.com.backend.business.dao.CompanyDao;
